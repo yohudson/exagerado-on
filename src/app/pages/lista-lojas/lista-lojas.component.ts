0e43d71ffeac3cc3fe2e77760687340d43ffc94d
Hudson Coelho <hudson.jgc@gmail.com>
2022-02-23 14:19:11 -0300

    catálogo de lojas

--- src/app/pages/lista-lojas/lista-lojas.component.ts
+++ src/app/pages/lista-lojas/lista-lojas.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-lojas.component.css']
 })
 export class ListaLojasComponent implements OnInit {
+  listaMarcas: any = []
 
   constructor() { }
 
   ngOnInit(): void {
+    this.listaMarcas =[
+      {nome: 'Juquita', corredor:'', loja:'Praça de alimentação', segmento:'Variedades'},
+      {nome: 'Cervejaria', corredor:'', loja:'Praça de alimentação', segmento:'Alimentação'},
+      {nome: 'Hamburgeria', corredor:'', loja:'Praça de alimentação', segmento:'Alimentação'},
+      {nome: 'Creperia', corredor:'', loja:'Praça de alimentação', segmento:'Alimentação'},
+      {nome: 'Salgaderia', corredor:'', loja:'Praça de alimentação', segmento:'Alimentação'},
+      {nome: 'Camiseteria', corredor:'E', loja:'5', segmento:'Alimentação'}
+    ]
   }
-
 }
