d25217fe5a9cdaed9e0c482467f52b9d82447654
Hudson Coelho <hudson.jgc@gmail.com>
2022-05-30 11:17:38 -0300

    atualização firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAdbaKlHXf8rFOjgZJiZqQSCoMB8srCwDw",
+    authDomain: "exagerado-on.firebaseapp.com",
+    projectId: "exagerado-on",
+    storageBucket: "exagerado-on.appspot.com",
+    messagingSenderId: "74678432779",
+    appId: "1:74678432779:web:c58628b706ddf79c30c30e",
+    measurementId: "G-B2VX9PJ9W2"
+  }
 };
